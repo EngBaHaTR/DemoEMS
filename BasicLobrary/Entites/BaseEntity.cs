03ae135288d8209feac4723beca873df84074ce5
BaHa.TR <eng.baha.tr@gmail.com>
2024-06-02 14:21:13 +0300

    Modele database

---
+++ BasicLobrary/Entites/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper.Configuration.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BasicLobrary.Entites
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        //Relationship: One To Many
+        [JsonIgnore]
+        public List<Employee> Employees { get; set; }
+    }
+}
